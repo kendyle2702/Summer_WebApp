cda6a5004942d05dcf447dc89d1cbc0b0c26431d
kendyle2702 <kendyle2702@gmai.com>
2023-11-02 18:00:03 +0700

    init commit

---
+++ Summer_WebApp/src/main/java/DAOs/AccountDAO.java
@@ -0,0 +1,59 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DAOs;
+
+import Models.Account;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author QuocCu
+ */
+public class AccountDAO {
+    private Connection conn;
+
+    public AccountDAO() {
+        try {
+            conn = DB.DBConnection.connect();
+        } catch (SQLException ex) {
+            Logger.getLogger(AccountDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public ResultSet getAll() {
+        ResultSet rs = null;
+        try {
+            Statement st = conn.createStatement();
+            rs = st.executeQuery("select * from Account");
+        } catch (SQLException ex) {
+            Logger.getLogger(AccountDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return rs;
+    }
+    
+    public Account getAccountbyUsernameAndPassword(String username, String password){
+        Account account = null;
+        try {
+            PreparedStatement ps = conn.prepareStatement("select * from Account where email = ? and password = ?");
+            ps.setString(1, username);
+            ps.setString(2, password);
+            ResultSet rs = ps.executeQuery();
+            if(rs.next()){
+                account = new Account(rs.getString("email"),rs.getString("password"),rs.getString("fullName"),rs.getDate("birhdate"),rs.getString("role"));
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(AccountDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return account;
+    }
+    
+    
+}
---
+++ Summer_WebApp/src/main/java/Models/Account.java
@@ -0,0 +1,73 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Models;
+
+import java.sql.Date;
+
+/**
+ *
+ * @author QuocCu
+ */
+public class Account {
+    private String email;
+    private String password;
+    private String fullName;
+    private Date birthdate;
+    private String role;
+
+    public Account(String email, String password, String fullName, Date birthdate, String role) {
+        this.email = email;
+        this.password = password;
+        this.fullName = fullName;
+        this.birthdate = birthdate;
+        this.role = role;
+    }
+
+    public Account() {
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public Date getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(Date birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+    
+    
+    
+}
